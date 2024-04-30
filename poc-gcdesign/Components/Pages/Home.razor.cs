8d38cd59279e1c3d11100755fb3140f7c1fe6f19
pandurx <internalfire90@gmail.com>
2024-04-29 22:52:23 -0400

    implemented with fluent validation

--- poc-gcdesign/Components/Pages/Home.razor.cs
+++ poc-gcdesign/Components/Pages/Home.razor.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using System.Reflection.Metadata;
 
@@ -18,15 +19,32 @@ namespace poc_gcdesign.Components.Pages
         //public string Error { get;set; } = string.Empty;
         public List<FieldError> FieldErrors { get; set; } = new List<FieldError>();
 
+        private EditContext _context;
+
+        private CustomerValidator _validator { get; set; }
+        private FluentValidation.Results.ValidationResult _validationResults { get; set; }
+
         protected override void OnInitialized()
         {
-            // empty
+            _context = new EditContext(Person);
+            _context.OnFieldChanged += _context_OnFieldChanged;
+
+            _validator = new CustomerValidator();
+            _validationResults = new FluentValidation.Results.ValidationResult();
+        }
+
+        private void _context_OnFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            if (!_context.Validate())
+            {
+                var fieldIdentifier = e.FieldIdentifier.FieldName;
+                var test = 1;
+            }
         }
 
         public async void SubmitForm()
         {
             var test1 = Person?.Surname;
-            var isValid = await _fluentValidationValidator!.ValidateAsync();
             FieldErrors.Add(
                 new FieldError()
                 {
@@ -43,11 +61,23 @@ namespace poc_gcdesign.Components.Pages
             var test2 = 1;
         }
 
+        public async void InvalidSubmission(EditContext context)
+        {
+            //var result = ValidatorConfiguration.Validate(RequestModel)
+            //var result = context.GetValidationMessages();
+            _validationResults = _validator.Validate(Person);
+            var result1 = _validationResults.Errors.Where(x => x.PropertyName.Equals("Surname"));
+            var joined = string.Join(", ", result1);
+            var test = 1;
+        }
+
         public string DisplayErrors(string field)
         {
-            var errors = FieldErrors.FirstOrDefault(x => x.Field == field);
+            //var errors = FieldErrors.FirstOrDefault(x => x.Field == field);
 
-            var ret = (errors is not null) ? string.Join(", ", errors.Errors) : string.Empty;
+            //var ret = (errors is not null) ? string.Join(", ", errors.Errors) : string.Empty;
+            var result = _validationResults.Errors.Where(x => x.PropertyName.Equals(field));
+            var ret = string.Join(", ", result);
             return ret;
         }
 
@@ -70,6 +100,7 @@ namespace poc_gcdesign.Components.Pages
             {
                 RuleFor(x => x.Surname)
                     .MaximumLength(5).WithMessage("Cannot exceed 5 characters")
+                    .Length(10).WithMessage("Length is 5")
                     .NotNull().WithMessage("Must be populated");
 
                 RuleFor(x => x.PhoneNumber)
